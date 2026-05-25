a95d64cc3ce6a96d19702411eeac32b6e117f52a
Vientousky <jotamancuello2048819@gmail.com>
2026-05-25 18:10:42 -0300

    v00.00.13

--- backend/models/db.js
+++ backend/models/db.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT || 5432,
+  port: process.env.PORT || 5432,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: false },
